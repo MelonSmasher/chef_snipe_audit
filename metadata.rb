fd604373e55d52faac0b73d8e135c2ef94d7373b
Alex Markessinis <markea125@gmail.com>
2018-06-21 14:45:37 -0400

    fixing gem errors

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Adds PC information to Snipe-IT.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.com/MelonSmasher/chef_snipe_audit/issues'
 source_url 'https://github.com/MelonSmasher/chef_snipe_audit'
-version '0.1.2'
+version '0.1.3'
 chef_version ">= 12" if respond_to?(:chef_version)
 supports 'windows'
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,19 +5,66 @@
 # MIT
 #
 
+# Install required libs
+chef_gem 'wmi-lite' do
+  version '1.0'
+  compile_time true
+  action :install
+end
+
+chef_gem 'http' do
+  version '3.3'
+  compile_time true
+  action :install
+end
+
+# Import required libs
+require 'http'
+require 'wmi-lite'
+
+# Our functions
+def get_asset_by_serial(base_url, token, serial)
+  HTTP[:accept => "application/json"]
+      .auth("Bearer #{token}")
+      .get("#{base_url}/hardware/byserial/#{serial}")
+end
+
+def get_asset_models(base_url, token)
+  HTTP[:accept => "application/json"]
+      .auth("Bearer #{token}")
+      .get("#{base_url}/models?limit=500&sort=id&order=asc")
+end
+
+def post_asset(base_url, token, data)
+  HTTP["Content-Type" => "application/json"]
+      .auth("Bearer #{token}")
+      .patch("#{base_url}/hardware", :json => data)
+end
+
+def patch_asset(base_url, token, data, id)
+  HTTP["Content-Type" => "application/json"]
+      .auth("Bearer #{token}")
+      .patch("#{base_url}/hardware/#{id}", :json => data)
+end
+
+def get_model_id_from_model_number(models, model_name)
+  models.each do |model|
+    if model['model_number'].equal?(model_name)
+      return model['id']
+    end
+  end
+  return nil
+end
+
+def build_base_url(hostname, port, use_https)
+  if hostname.to_s.empty? or port.to_s.empty?
+    return false
+  end
+  use_https ? "https://#{hostname}:#{port.to_s}" : "http://#{hostname}:#{port.to_s}"
+end
+
 # Make sure we are on Windows
 if node['platform_family'].to_s.downcase === 'windows' and RUBY_PLATFORM =~ /mswin|mingw32|windows/
-
-  # Install required libs
-  chef_gem 'wmi-lite' do
-    version '1.0'
-    compile_time true
-    action :install
-  end
-
-  # Import required libs
-  require '../libraries/helpers'
-  require 'wmi-lite'
 
   # Store Snipe info in vars for later
   base_url = base_url(node['snipe']['host_name'], node['snipe']['port'], node['snipe']['use_https'])
