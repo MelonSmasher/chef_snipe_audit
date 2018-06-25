bea3b1564dde5c854f3d7218482eea6ff0809f0c
Alex Markessinis <markea125@gmail.com>
2018-06-25 15:52:23 -0400

    fixing client

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,4 @@ default['snipe']['server']['use_https'] = false
 default['snipe']['user']['api_token'] = ''
 default['snipe']['fields']['os']['name'] = false
 default['snipe']['fields']['os']['version'] = false
+default['snipe']['fields']['new_asset_status']['id'] = 1
